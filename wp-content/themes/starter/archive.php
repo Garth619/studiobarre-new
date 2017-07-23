1d79ff03ad411e04aba8facc5bc81ae96e6e5168
Garrett Cullen <garrettcullen@garretts-MacBook-Pro.local>
2017-07-23 15:04:31 -0700

    Push

--- wp-content/themes/starter/archive.php
+++ wp-content/themes/starter/archive.php
@@ -1,22 +1,6 @@
-<?php
-/**
- * Template for displaying Archive pages
- *
- * Used to display archive-type pages if nothing more specific matches a query.
- * For example, puts together date-based pages if no date.php file exists.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package WordPress
- * @subpackage Twenty_Ten
- * @since Twenty Ten 1.0
- */
+<?php get_header(); ?>
 
-get_header(); ?>
-
-		<div id="container">
-			<div id="content" role="main">
-
+		
 <?php
 	/*
 	 * Queue the first post, that way we know
@@ -57,8 +41,7 @@ get_header(); ?>
 	get_template_part( 'loop', 'archive' );
 ?>
 
-			</div><!-- #content -->
-		</div><!-- #container -->
 
-<?php get_sidebar(); ?>
+
+<?php // get_sidebar(); ?>
 <?php get_footer(); ?>
--- wp-content/themes/starter/category.php
+++ wp-content/themes/starter/category.php
@@ -1,20 +1,12 @@
-<?php
-/**
- * Template for displaying Category Archive pages
- *
- * @package WordPress
- * @subpackage Twenty_Ten
- * @since Twenty Ten 1.0
- */
+<?php get_header(); ?>
 
-get_header(); ?>
 
-		<div id="container">
-			<div id="content" role="main">
 
 				<h1 class="page-title"><?php
 					printf( __( 'Category Archives: %s', 'twentyten' ), '<span>' . single_cat_title( '', false ) . '</span>' );
 				?></h1>
+				
+				
 				<?php
 					$category_description = category_description();
 					if ( ! empty( $category_description ) )
@@ -28,8 +20,7 @@ get_header(); ?>
 				get_template_part( 'loop', 'category' );
 				?>
 
-			</div><!-- #content -->
-		</div><!-- #container -->
+		
 
-<?php get_sidebar(); ?>
+<?php // get_sidebar(); ?>
 <?php get_footer(); ?>
