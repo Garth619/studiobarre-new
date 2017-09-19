77b1a3a445cfc9e0baee702bded2184f1d73d9b7
Garrett Cullen <Garth619@users.noreply.github.com>
2017-09-18 20:32:32 -0700

    push

--- wp-content/themes/studiobarrecorporate/sidebar-blog.php
+++ wp-content/themes/studiobarrecorporate/sidebar-blog.php
@@ -3,6 +3,33 @@
 	
 	<h1 class="header_script">Blog</h1><!-- header_script -->
 	
+	<span class="latest_posts">Latest Posts</span>
+	
+	
+	<ul class="latest_post_list">
+	
+	<?php $mymain_query = new WP_Query( array( 'post_type' => 'post' ,'posts_per_page' => '4', 'order' => 'DSC' ) ); while($mymain_query->have_posts()) : $mymain_query->the_post(); ?>
+	
+	
+			
+		<li>
+			
+			<span class="sidebar_date"><?php $pfx_date = get_the_date();echo  $pfx_date;?></span>
+			
+			<a class="" href="<?php the_permalink();?>"><?php the_title();?></a>
+		
+		</li>
+     
+    
+    
+    <?php endwhile; ?>
+   <?php wp_reset_postdata(); // reset the query ?>
+	
+	</ul>
+	
+	
+	
+		
 		
 </div><!-- sidebar -->
 			
