5d290eefb083b48b8a1c5ce37504aeb42bb6187a
Garrett Cullen <Garth619@users.noreply.github.com>
2017-09-20 23:06:48 -0700

    push

--- wp-content/themes/studiobarrecorporate/sidebar-maven.php
+++ wp-content/themes/studiobarrecorporate/sidebar-maven.php
@@ -7,6 +7,8 @@
 			
 			<span class="maven_form_title">Apply to be a bar maven</span><!-- maven_form_title -->
 			
+			<h1 class="maven_mobile_title">Barre Mavens</h1>
+			
 			<?php gravity_form(3, false, false, false, '', true, 12); ?>
 			
 		</div><!-- sidebar_inner_form_wrapper -->
