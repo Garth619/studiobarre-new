71b285e1c1a3a2461c6cfc94c71be980a3ae8f5c
Garrett Cullen <Garth619@users.noreply.github.com>
2017-09-24 16:06:11 -0700

    push

--- wp-content/themes/studiobarrezee/sidebar.php
+++ wp-content/themes/studiobarrezee/sidebar.php
@@ -21,9 +21,47 @@
 			<li><a href="">Events</a></li>
 		</ul>
 		
-		
-		
 	</div><!-- location_menu -->
+	
+	<div class="sidebar_social">
+		
+		<a href="" target="_blank"><img src="<?php bloginfo('template_directory');?>/images/twitter.png"/></a>
+		<a href="" target="_blank"><img src="<?php bloginfo('template_directory');?>/images/pinterest.png"/></a>
+		<a href="" target="_blank"><img src="<?php bloginfo('template_directory');?>/images/instagram.png"/></a>
+		<a href="" target="_blank"><img src="<?php bloginfo('template_directory');?>/images/facebook.png"/></a>
+		<a href="" target="_blank"><img src="<?php bloginfo('template_directory');?>/images/google-plus.png"/></a>
+		<a class="" href="mailto:"><img class="email" src="<?php bloginfo('template_directory');?>/images/email.png"/></a>
+		
+	</div><!-- sidebar_social -->
+	
+	<div class="store_hours">
+		
+		<span class="hours_title">Store Hours</span><!-- hours_title -->
+		
+		<span class="days">Monday - Thursday</span><!-- days -->
+		<span class="hours">8am - 8pm</span><!-- days -->
+		
+		<span class="days">Friday</span><!-- days -->
+		<span class="hours">8am - 7pm</span><!-- days -->
+		
+		<span class="days">Saturday - Sunday</span><!-- days -->
+		<span class="hours">7am - 11am</span><!-- days -->
+		
+	</div><!-- store_hours -->
+	
+	<div class="address">
+		
+		<a href="" target="_blank">
+			7845 Highland Village Place,
+			Suite C-103
+			San Diego, CA 92129
+		</a>
+		
+		<a href="tel:858.365.7169">Phone: 858.365.7169</a>
+		
+		
+		
+	</div><!-- address -->
 	
 </div><!-- sidebar -->
 			
