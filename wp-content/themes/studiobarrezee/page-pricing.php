fe1e8bfd67fe2ad711377c10118dcdd2aea40e3a
Garrett Cullen <Garth619@users.noreply.github.com>
2017-09-26 20:55:08 -0700

    push

--- wp-content/themes/studiobarrezee/page-pricing.php
+++ wp-content/themes/studiobarrezee/page-pricing.php
@@ -31,25 +31,235 @@
 								
 								<span class="box_single_title">sTUDIO BARRE SOCIETY</span><!-- box_single_title -->
 								
-								<div class="box_single_content">
-									
-									
-									<span class="single_line_left">unlimited classes, exclusive sales and swag, 2 vacation pauses</span>
-									
-									<span class="single_line_right">$129.00</span><!-- single_line_right -->
-									
-									
-								</div><!-- box_single_content -->
+								
 								
 								<div class="box_single_content">
 									
+									<div class="single_line_wrapper">
 									
-									<span class="single_line_left">unlimited classes, exclusive sales and swag, 2 vacation pauses</span>
+										<span class="single_line_left">unlimited classes, exclusive sales and swag, 2 vacation pauses</span>
 									
-									<span class="single_line_right">$129.00</span><!-- single_line_right -->
+										<span class="single_line_right">$129.00</span><!-- single_line_right -->
+										
+									</div><!-- single_line_wrapper -->
 									
+									<span class="single_line">*30 days cancellation</span><!-- single_line -->
 									
 								</div><!-- box_single_content -->
+								
+								
+								<div class="box_single_content">
+									
+									<div class="single_line_wrapper">
+									
+										<span class="single_line_left">unlimited classes, exclusive sales and swag, 2 vacation pauses</span>
+									
+										<span class="single_line_right">$129.00</span><!-- single_line_right -->
+									
+									</div><!-- single_line_wrapper -->
+									
+								</div><!-- box_single_content -->
+								
+								
+								<div class="box_single_content">
+									
+									<div class="single_line_wrapper">
+									
+										<span class="single_line_left">unlimited classes, exclusive sales and swag, 2 vacation pauses</span>
+									
+										<span class="single_line_right">$129.00</span><!-- single_line_right -->
+										
+										
+									
+									</div><!-- single_line_wrapper -->
+									
+								</div><!-- box_single_content -->
+								
+								
+								<div class="box_single_content">
+									
+									<div class="single_line_wrapper">
+									
+										<span class="single_line_left">unlimited classes, exclusive sales and swag, 2 vacation pauses</span>
+									
+										<span class="single_line_right">$129.00</span><!-- single_line_right -->
+									
+									</div><!-- single_line_wrapper -->
+									
+								</div><!-- box_single_content -->
+								
+								
+								
+								<span class="single_line_ast">*Vacation pauses must be a minimum of 2 weeks and a maximum of 4 weeks</span><!-- single_line_right -->
+								<span class="single_line_ast">*Vacation pauses must be a minimum of 2 weeks and a maximum of 4 weeks</span><!-- single_line_right -->
+								<span class="single_line_ast">*Vacation pauses must be a minimum of 2 weeks and a maximum of 4 weeks</span><!-- single_line_right -->
+								
+								
+								
+							</div><!-- box_single_description -->
+							
+						</div><!-- box_description_wrapper -->
+						
+					</div><!-- box_wrapper -->
+					
+					
+					<div class="box_wrapper">
+						
+						<div class="box_title_wrapper">Most Popular</div><!-- box_title_wrapper -->
+						
+						<div class="box_description_wrapper">
+							
+							<div class="box_single_description">
+								
+								<span class="box_single_title">sTUDIO BARRE SOCIETY</span><!-- box_single_title -->
+								
+								
+								
+								<div class="box_single_content">
+									
+									<div class="single_line_wrapper">
+									
+										<span class="single_line_left">unlimited classes, exclusive sales and swag, 2 vacation pauses</span>
+									
+										<span class="single_line_right">$129.00</span><!-- single_line_right -->
+										
+									</div><!-- single_line_wrapper -->
+									
+									<span class="single_line">*30 days cancellation</span><!-- single_line -->
+									
+								</div><!-- box_single_content -->
+								
+								
+								<div class="box_single_content">
+									
+									<div class="single_line_wrapper">
+									
+										<span class="single_line_left">unlimited classes, exclusive sales and swag, 2 vacation pauses</span>
+									
+										<span class="single_line_right">$129.00</span><!-- single_line_right -->
+									
+									</div><!-- single_line_wrapper -->
+									
+								</div><!-- box_single_content -->
+								
+								
+								<div class="box_single_content">
+									
+									<div class="single_line_wrapper">
+									
+										<span class="single_line_left">unlimited classes, exclusive sales and swag, 2 vacation pauses</span>
+									
+										<span class="single_line_right">$129.00</span><!-- single_line_right -->
+										
+										
+									
+									</div><!-- single_line_wrapper -->
+									
+								</div><!-- box_single_content -->
+								
+								
+								<div class="box_single_content">
+									
+									<div class="single_line_wrapper">
+									
+										<span class="single_line_left">unlimited classes, exclusive sales and swag, 2 vacation pauses</span>
+									
+										<span class="single_line_right">$129.00</span><!-- single_line_right -->
+									
+									</div><!-- single_line_wrapper -->
+									
+								</div><!-- box_single_content -->
+								
+								
+								
+								<span class="single_line_ast">*Vacation pauses must be a minimum of 2 weeks and a maximum of 4 weeks</span><!-- single_line_right -->
+								<span class="single_line_ast">*Vacation pauses must be a minimum of 2 weeks and a maximum of 4 weeks</span><!-- single_line_right -->
+								<span class="single_line_ast">*Vacation pauses must be a minimum of 2 weeks and a maximum of 4 weeks</span><!-- single_line_right -->
+								
+								
+								
+							</div><!-- box_single_description -->
+							
+						</div><!-- box_description_wrapper -->
+						
+					</div><!-- box_wrapper -->
+					
+					
+					
+					<div class="box_wrapper">
+						
+						<div class="box_title_wrapper">Most Popular</div><!-- box_title_wrapper -->
+						
+						<div class="box_description_wrapper">
+							
+							<div class="box_single_description">
+								
+								<span class="box_single_title">sTUDIO BARRE SOCIETY</span><!-- box_single_title -->
+								
+								
+								
+								<div class="box_single_content">
+									
+									<div class="single_line_wrapper">
+									
+										<span class="single_line_left">unlimited classes, exclusive sales and swag, 2 vacation pauses</span>
+									
+										<span class="single_line_right">$129.00</span><!-- single_line_right -->
+										
+									</div><!-- single_line_wrapper -->
+									
+									<span class="single_line">*30 days cancellation</span><!-- single_line -->
+									
+								</div><!-- box_single_content -->
+								
+								
+								<div class="box_single_content">
+									
+									<div class="single_line_wrapper">
+									
+										<span class="single_line_left">unlimited classes, exclusive sales and swag, 2 vacation pauses</span>
+									
+										<span class="single_line_right">$129.00</span><!-- single_line_right -->
+									
+									</div><!-- single_line_wrapper -->
+									
+								</div><!-- box_single_content -->
+								
+								
+								<div class="box_single_content">
+									
+									<div class="single_line_wrapper">
+									
+										<span class="single_line_left">unlimited classes, exclusive sales and swag, 2 vacation pauses</span>
+									
+										<span class="single_line_right">$129.00</span><!-- single_line_right -->
+										
+										
+									
+									</div><!-- single_line_wrapper -->
+									
+								</div><!-- box_single_content -->
+								
+								
+								<div class="box_single_content">
+									
+									<div class="single_line_wrapper">
+									
+										<span class="single_line_left">unlimited classes, exclusive sales and swag, 2 vacation pauses</span>
+									
+										<span class="single_line_right">$129.00</span><!-- single_line_right -->
+									
+									</div><!-- single_line_wrapper -->
+									
+								</div><!-- box_single_content -->
+								
+								
+								
+								<span class="single_line_ast">*Vacation pauses must be a minimum of 2 weeks and a maximum of 4 weeks</span><!-- single_line_right -->
+								<span class="single_line_ast">*Vacation pauses must be a minimum of 2 weeks and a maximum of 4 weeks</span><!-- single_line_right -->
+								<span class="single_line_ast">*Vacation pauses must be a minimum of 2 weeks and a maximum of 4 weeks</span><!-- single_line_right -->
+								
+								
 								
 							</div><!-- box_single_description -->
 							
