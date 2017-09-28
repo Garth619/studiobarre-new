f531856020117c735b21c9ffa9690d6b093b4117
Garrett Cullen <Garth619@users.noreply.github.com>
2017-09-27 21:58:05 -0700

    push

--- wp-content/themes/studiobarrezee/page-pricing.php
+++ wp-content/themes/studiobarrezee/page-pricing.php
@@ -45,52 +45,7 @@
 										
 									</div><!-- single_line_wrapper -->
 									
-								</div><!-- box_single_content -->
-								
-						
-								<span class="single_line_ast">*Vacation pauses must be a minimum of 2 weeks and a maximum of 4 weeks</span><!-- single_line_right -->
-								
-								
-							</div><!-- box_single_description -->
-							
-							</div><!-- box_description_inner_wrapper -->
-							
-							
-							
-							
-							
-						</div><!-- box_description_wrapper -->
-						
-						
-						<a class="buy_now" href="">Buy Now</a><!-- buy_now -->
-						
-						
-					</div><!-- box_wrapper -->
-					
-					
-					<div class="box_wrapper">
-						
-						<div class="box_title_wrapper">Most Popular</div><!-- box_title_wrapper -->
-						
-						<div class="box_description_wrapper">
-							
-							<div class="box_description_inner_wrapper">
-							
-							<div class="box_single_description">
-								
-								<span class="box_single_title">sTUDIO BARRE SOCIETY</span><!-- box_single_title -->
-								
-								
-								
-								<div class="box_single_content">
-									
-									<div class="single_line_wrapper">
-									
-										<span class="single_line_left">unlimited classes, exclusive sales and swag, 2 vacation pauses</span>
-									
-										<span class="single_line_right">$129.00</span><!-- single_line_right -->
-										
-									</div><!-- single_line_wrapper -->
+									<span class="single_line">*30 days cancellation.</span><!-- single_line -->
 									
 								</div><!-- box_single_content -->
 								
@@ -117,7 +72,7 @@
 					
 					<div class="box_wrapper">
 						
-						<div class="box_title_wrapper">Most Popular</div><!-- box_title_wrapper -->
+						<div class="box_title_wrapper">New to the Studio</div><!-- box_title_wrapper -->
 						
 						<div class="box_description_wrapper">
 							
@@ -125,7 +80,7 @@
 							
 							<div class="box_single_description">
 								
-								<span class="box_single_title">sTUDIO BARRE SOCIETY</span><!-- box_single_title -->
+								<span class="box_single_title">NEW CLIENT SPECIAL</span><!-- box_single_title -->
 								
 								
 								
@@ -133,16 +88,16 @@
 									
 									<div class="single_line_wrapper">
 									
-										<span class="single_line_left">unlimited classes, exclusive sales and swag, 2 vacation pauses</span>
+										<span class="single_line_left">one month of unlimited classes</span>
 									
-										<span class="single_line_right">$129.00</span><!-- single_line_right -->
+										<span class="single_line_right">$99.00</span><!-- single_line_right -->
 										
 									</div><!-- single_line_wrapper -->
 									
 								</div><!-- box_single_content -->
 								
 						
-								<span class="single_line_ast">*Vacation pauses must be a minimum of 2 weeks and a maximum of 4 weeks</span><!-- single_line_right -->
+								<span class="single_line_ast">* available for new clients only or those who haven't taken class in 6 months or more</span><!-- single_line_right -->
 								
 								
 							</div><!-- box_single_description -->
@@ -163,6 +118,9 @@
 					
 					
 					
+					
+					
+					
 					<div class="reg_price_wrapper">
 						
 						<span class="reg_price_title">New Client Special</span>
@@ -175,16 +133,62 @@
 							
 						</div><!-- reg_single_line_wrapper -->
 						
+						
+						<span class="single_line">Lorem Ipsum</span><!-- single_line_right -->
+						
+						
+						<span class="reg_ast">*first time clients only</span><!-- single_line_right -->
+						
+					</div><!-- reg_price_wrapper -->
+					
+					
+					<div class="reg_price_wrapper">
+						
+						<span class="reg_price_title">monthly rates</span>
+						
 						<div class="reg_single_line_wrapper">
 							
-							<span class="single_line_left">2 weeks unlimited</span>
+							<span class="single_line_left">1 month</span>
 									
-							<span class="single_line_right">$59.00</span><!-- single_line_right -->
+							<span class="single_line_right">$200 per month</span><!-- single_line_right -->
 							
 						</div><!-- reg_single_line_wrapper -->
-
+						
+						
+						
+					</div><!-- reg_price_wrapper -->
+					
+					
+					<div class="reg_price_wrapper">
+						
+						<span class="reg_price_title">student / teacher/ military discount package</span>
+						
+						<div class="reg_single_line_wrapper">
 							
-						<span class="single_line">Lorem Ipsum</span><!-- single_line_right -->
+							<span class="single_line_left">10 classes</span>
+									
+							<span class="single_line_right">$125</span><!-- single_line_right -->
+							
+						</div><!-- reg_single_line_wrapper -->
+						
+						
+						
+					</div><!-- reg_price_wrapper -->
+					
+					
+					<div class="reg_price_wrapper">
+						
+						<span class="reg_price_title">new mother {tucker} special</span>
+						
+						<div class="reg_single_line_wrapper">
+							
+							<span class="single_line_left">3 months</span>
+									
+							<span class="single_line_right">$375.00*</span><!-- single_line_right -->
+							
+						</div><!-- reg_single_line_wrapper -->
+						
+						
 						
 						
 						<span class="reg_ast">*this package is available for all new mothers within 6 months of their delivery.  enjoy this monthly bounce back package …$125.00 a month..what a great deal!</span><!-- single_line_right -->
@@ -192,89 +196,58 @@
 					</div><!-- reg_price_wrapper -->
 					
 					
+					
 					<div class="reg_price_wrapper">
 						
-						<span class="reg_price_title">New Client Special</span>
+						<span class="reg_price_title">i tuck, i sweat, i do {bride to be package}</span>
 						
 						<div class="reg_single_line_wrapper">
 							
-							<span class="single_line_left">2 weeks unlimited</span>
+							<span class="single_line_left">3 months unlimited</span>
 									
-							<span class="single_line_right">$59.00</span><!-- single_line_right -->
+							<span class="single_line_right">$375.00*</span><!-- single_line_right -->
 							
 						</div><!-- reg_single_line_wrapper -->
 						
-						<div class="reg_single_line_wrapper">
-							
-							<span class="single_line_left">2 weeks unlimited</span>
-									
-							<span class="single_line_right">$59.00</span><!-- single_line_right -->
-							
-						</div><!-- reg_single_line_wrapper -->
-
-							
-						<span class="single_line">Lorem Ipsum</span><!-- single_line_right -->
-						
-						
-						<span class="reg_ast">*this package is available for all new mothers within 6 months of their delivery.  enjoy this monthly bounce back package …$125.00 a month..what a great deal!</span><!-- single_line_right -->
 						
 					</div><!-- reg_price_wrapper -->
 					
 					
-					<div class="reg_price_wrapper">
-						
-						<span class="reg_price_title">New Client Special</span>
-						
-						<div class="reg_single_line_wrapper">
-							
-							<span class="single_line_left">2 weeks unlimited</span>
-									
-							<span class="single_line_right">$59.00</span><!-- single_line_right -->
-							
-						</div><!-- reg_single_line_wrapper -->
-						
-						<div class="reg_single_line_wrapper">
-							
-							<span class="single_line_left">2 weeks unlimited</span>
-									
-							<span class="single_line_right">$59.00</span><!-- single_line_right -->
-							
-						</div><!-- reg_single_line_wrapper -->
-
-							
-						<span class="single_line">Lorem Ipsum</span><!-- single_line_right -->
-						
-						
-						<span class="reg_ast">*this package is available for all new mothers within 6 months of their delivery.  enjoy this monthly bounce back package …$125.00 a month..what a great deal!</span><!-- single_line_right -->
-						
-					</div><!-- reg_price_wrapper -->
 					
 					
 					<div class="reg_price_wrapper">
 						
-						<span class="reg_price_title">New Client Special</span>
+						<span class="reg_price_title">packages</span>
 						
 						<div class="reg_single_line_wrapper">
 							
-							<span class="single_line_left">2 weeks unlimited</span>
+							<span class="single_line_left">1 class</span>
 									
-							<span class="single_line_right">$59.00</span><!-- single_line_right -->
+							<span class="single_line_right">$20 { $20 per class }</span><!-- single_line_right -->
 							
 						</div><!-- reg_single_line_wrapper -->
 						
 						<div class="reg_single_line_wrapper">
 							
-							<span class="single_line_left">2 weeks unlimited</span>
+							<span class="single_line_left">10 classes</span>
 									
-							<span class="single_line_right">$59.00</span><!-- single_line_right -->
+							<span class="single_line_right">$180 { $18 per class }</span><!-- single_line_right -->
 							
 						</div><!-- reg_single_line_wrapper -->
-
+						
+						
+						<div class="reg_single_line_wrapper">
 							
-						<span class="single_line">Lorem Ipsum</span><!-- single_line_right -->
+							<span class="single_line_left">20 classes</span>
+									
+							<span class="single_line_right">$340 { $17 per class }</span><!-- single_line_right -->
+							
+						</div><!-- reg_single_line_wrapper -->
 						
+						<span class="single_line">looking to try studio barre for the first time??</span><!-- single_line_right -->
 						
-						<span class="reg_ast">*this package is available for all new mothers within 6 months of their delivery.  enjoy this monthly bounce back package …$125.00 a month..what a great deal!</span><!-- single_line_right -->
+						<span class="reg_ast">*first class free for all san diegans</span><!-- single_line_right -->
+						
 						
 					</div><!-- reg_price_wrapper -->
 										
