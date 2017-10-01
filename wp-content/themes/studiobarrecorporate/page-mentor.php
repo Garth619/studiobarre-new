89bb60d5dd053cb3f988b73494763ba0a26fe17f
Garrett Cullen <Garth619@users.noreply.github.com>
2017-10-01 12:46:03 -0700

    push

--- wp-content/themes/studiobarrecorporate/page-mentor.php
+++ wp-content/themes/studiobarrecorporate/page-mentor.php
@@ -24,13 +24,80 @@
 			
 			<div class="content_wrapper content">
 			
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<?php the_field( 'mentor_content' ); ?>
 				
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-				
-				<h2>Meet the Studio Barre {Mentors}</h2>
+				<h2><?php the_field( 'meet_the_mentors_title' ); ?></h2>
 					
-				<?php get_template_part('bio','grid');?>
+				
+				<?php if(get_field('bio_grid_mentors')): ?>
+
+	<div class="bio_grid">
+ 
+	<?php while(has_sub_field('bio_grid_mentors')): ?>
+ 
+		
+		
+				<div class="single_bio_grid">
+						
+						<div class="image_wrapper">
+							
+							<?php 
+							
+								$imageIDbw = get_sub_field('black_and_white_image');
+								$alt_textbw = get_post_meta($imageIDbw , '_wp_attachment_image_alt', true); 
+								$result_onebw = wp_get_attachment_image_src($imageIDbw, 'bioimage');
+								$imageID = get_sub_field('bio_image');
+								$alt_text = get_post_meta($imageID , '_wp_attachment_image_alt', true);
+								$result_one = wp_get_attachment_image_src($imageID, 'bioimage');
+							
+							?>
+							
+							<img class="black_white" src="<?php echo $result_onebw[0]; ?>" alt="<?php echo $alt_textbw; ?>"/>
+							
+							<img class="color" src="<?php echo $result_one[0]; ?>" alt="<?php echo $alt_text; ?>"/>
+							
+						</div><!-- image_wrapper -->
+						
+						<div class="bio_content">
+							
+							<h2 class="bio_name"><?php the_sub_field( 'name' ); ?></h2>
+							
+							<span class="location"><a href="<?php the_sub_field( 'location_link' ); ?>">{<?php the_sub_field( 'location' ); ?>}</a></span><!-- bio_name -->
+							
+ 							<span class="quote"><?php the_sub_field( 'quote' ); ?></span><!-- bio_position -->
+							
+							<span class="read_bio">Read Bio</span><!-- read_bio -->
+							
+						</div><!-- bio_content -->
+						
+						<div class="bio_overlay_bg">
+							
+							<div class="bio_overlay_wrapper">
+								
+								<span class="bio_overlay_close">X</span><!-- close -->
+								
+								<h2><?php the_sub_field( 'name' ); ?></h2>
+								
+								<?php the_sub_field( 'bio_content' ); ?>								
+								
+								
+								<?php the_sub_field( 'connect' ); ?>
+								
+							</div><!-- bio_overlay_wrapper -->
+							
+						</div><!-- bio_overlay_bg -->
+						
+					</div><!-- single_bio_grid -->
+		
+		
+		
+    
+	<?php endwhile; ?>
+ 
+	</div><!-- bio_grid -->
+ 
+<?php endif; ?>
+
 				
 				
 			</div><!-- content -->
